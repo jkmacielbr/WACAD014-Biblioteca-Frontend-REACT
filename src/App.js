6f081ceb107ffc92598921e4b82437581f64d835
jkmacielbr <jk.maciel.br@gmail.com>
2023-07-16 16:00:53 -0400

    utilizando props no card

--- src/App.js
+++ src/App.js
@@ -2,24 +2,52 @@ import React from 'react'
 import Table from './Table'
 import Navbar from './Nav'
 import Card from './Card'
+import logo1 from './utils/logo1.png';
+import logo2 from './utils/logo2.png';
+import logo3 from './utils/logo3.jpeg';
+import logo4 from './utils/logo4.png';
+import logo5 from './utils/logo5.png';
+import logo6 from './utils/logo6.png';
 
 
 
-class App extends React.Component {
+
+
+  class App extends React.Component {
     render() {
       return (
-        
         <div className="container text-center">
-          
           <Navbar/>
           <br/>
           <h1>STORE DAY!</h1>
           <br></br>
-          <Card/>
-          {/* <Table /> */}
-         
+        
+        <div className="container text-center">
+          <br></br>
+          <div className="row">
+            <div className="col-md-4">
+              <Card logo={logo1} title="Camisa 1 CR FLAMENGO 23/24" price="R$349,75" />
+            </div>
+            <div className="col-md-4">
+              <Card logo={logo2} title="Camisa 3 FLAMENGO" price="R$390,00" />
+            </div>
+            <div className="col-md-4">
+              <Card logo={logo3} title="Camisa 1 CR FLAMENGO 22/23" price="R$230,00" />
+            </div>
+            <div className="col-md-4">
+              <Card logo={logo4} title="Camisa FLAMENGO II 23/24" price="R$284,00" />
+            </div>
+            <div className="col-md-4">
+              <Card logo={logo5} title="Camisa FLAMENGO 23" price="R$184,00" />
+            </div>
+            <div className="col-md-4">
+              <Card logo={logo5} title="Camisa FLAMENGO Retro" price="R$205,00" />
+            </div>
+           
+          </div>
         </div>
-      )
+        </div>
+      );
     }
   }
   
--- src/Card.js
+++ src/Card.js
@@ -8,140 +8,37 @@ import logo6 from './utils/logo6.png'
 
 class Card extends Component {
   render() {
+    const {logo, title, price} = this.props;
     return (
-      <div class="row row-cols-1 row-cols-md-3 g-6">
-        <div class="col">
-  <div class="card h-100">
-    <svg
-      class="bd-placeholder-img card-img-top"
-      width="100%"
-      height="140"
-      xmlns="http://www.w3.org/2000/svg"
-      role="img"
-      aria-label="Placeholder: Image cap"
-      preserveAspectRatio="xMidYMid slice"
-      focusable="false"
-    >
-      <title>Placeholder</title>
-      
-      <image href={logo1} width="100%" height="100%" />
-      
-    </svg>
-    <div class="card-body">
-      <h5 class="card-title">Camisa 1 CR FLAMENGO 23/24</h5>
-      <a href="#" class="btn btn-primary">R$349,75</a>
-    </div>
-  </div>
-</div>
+      <div className="row row-cols-1 row-cols-md-3 g-6">
+        <div className="col">
+          <div className="card h-100">
+            <svg
+              className="bd-placeholder-img card-img-top"
+              width="100%"
+              height="140"
+              xmlns="http://www.w3.org/2000/svg"
+              role="img"
+              aria-label="Placeholder: Image cap"
+              preserveAspectRatio="xMidYMid slice"
+              focusable="false"
+            >
+              <title>Placeholder</title>
 
-        <div class="col">
-          <div class="card h-100">
-            <svg
-              class="bd-placeholder-img card-img-top"
-              width="100%"
-              height="140"
-              xmlns="http://www.w3.org/2000/svg"
-              role="img"
-              aria-label="Placeholder: Image cap"
-              preserveAspectRatio="xMidYMid slice"
-              focusable="false"
-            >
-              <title>Placeholder</title>
-              <image href={logo2} width="100%" height="100%" />
+              <image href={logo} width="100%" height="100%" />
+
             </svg>
-            <div class="card-body">
-              <h5 class="card-title">Camisa 3 FLAMENGO </h5>
-              <a href="#" class="btn btn-primary">R$390,00</a>
+            <div className="card-body">
+              <h5 className="card-title">{title}</h5>
+              <a href="#" class="btn btn-primary">{price}</a>
             </div>
           </div>
         </div>
-        <div class="col">
-          <div class="card h-100">
-            <svg
-              class="bd-placeholder-img card-img-top"
-              width="100%"
-              height="140"
-              xmlns="http://www.w3.org/2000/svg"
-              role="img"
-              aria-label="Placeholder: Image cap"
-              preserveAspectRatio="xMidYMid slice"
-              focusable="false"
-            >
-              <title>Placeholder</title>
-              <image href={logo3} width="100%" height="100%" />
-            </svg>
-            <div class="card-body">
-              <h5 class="card-title">Camisa 1 CR FLAMENGO 22</h5>
-              <a href="#" class="btn btn-primary">R$230,00</a>
-            </div>
-          </div>
         </div>
-        <div class="col">
-          <div class="card h-100">
-            <svg
-              class="bd-placeholder-img card-img-top"
-              width="100%"
-              height="140"
-              xmlns="http://www.w3.org/2000/svg"
-              role="img"
-              aria-label="Placeholder: Image cap"
-              preserveAspectRatio="xMidYMid slice"
-              focusable="false"
-            >
-              <title>Placeholder</title>
-              <image href={logo4} width="100%" height="100%" />
-            </svg>
-            <div class="card-body">
-              <h5 class="card-title">Camisa FLAMENGO II 23/24</h5>
-              <a href="#" class="btn btn-primary">R$284,00</a>
-            </div>
-          </div>
-        </div>
-        <div class="col">
-          <div class="card h-100">
-            <svg
-              class="bd-placeholder-img card-img-top"
-              width="100%"
-              height="140"
-              xmlns="http://www.w3.org/2000/svg"
-              role="img"
-              aria-label="Placeholder: Image cap"
-              preserveAspectRatio="xMidYMid slice"
-              focusable="false"
-            >
-              <title>Placeholder</title>
-              <image href={logo5} width="100%" height="100%" />
-            </svg>
-            <div class="card-body">
-              <h5 class="card-title">Camisa Flamengo 23</h5>
-              <a href="#" class="btn btn-primary">R$184,00</a>
-            </div>
-          </div>
-        </div>
-        <div class="col">
-          <div class="card h-100">
-            <svg
-              class="bd-placeholder-img card-img-top"
-              width="100%"
-              height="140"
-              xmlns="http://www.w3.org/2000/svg"
-              role="img"
-              aria-label="Placeholder: Image cap"
-              preserveAspectRatio="xMidYMid slice"
-              focusable="false"
-            >
-              <title>Placeholder</title>
-              <image href={logo6} width="100%" height="100%" />
-            </svg>
-            <div class="card-body">
-              <h5 class="card-title">Camisa FLAMENGO Retro</h5>
-              <a href="#" class="btn btn-primary">R$205,90</a>
-            </div>
-          </div>
-        </div>
-      </div>
+       
     )
-  }
-}
+    }
+    }
+  
 
-export default Card
+export default Card;
